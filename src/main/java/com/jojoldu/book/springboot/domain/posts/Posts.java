01f20dc13b14678597209821098cdf52d9322e9a
abcturtle <pkt91186@gmail.com>
2024-01-28 17:52:46 +0900

    page 161

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,8 @@ import javax.persistence.*;
 // public Posts() {} 와 같은 효과
 @Entity
 // 테이블과 링크될 클래스
-public class Posts {
+// Posts 클래스가 BaseTimeEntity 를 상속받도록 변경.
+public class Posts extends BaseTimeEntity {
     // 실제 DB 의 테이블과 매칭될 클래스 이며 보통 Entity 클래스라고도 합니다 .
     // JPA 를 사용하게되면 DB 데이터에 작업할 경우 실제쿼리를 날리기보다는, 이 Entity 클래스의 수정을 통해 작업한다.
 
@@ -34,5 +36,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update (String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
---
+++ src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
@@ -0,0 +1,40 @@
+package com.jojoldu.book.springboot.web;
+
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestsDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestsDto requestsDto) {
+        return postsService.save(requestsDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestsDto) {
+        return postsService.update(id, requestsDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+}
